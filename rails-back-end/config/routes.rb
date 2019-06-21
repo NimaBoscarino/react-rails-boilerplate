d1f0bf8c186fa76777b2724d4b376412399a7ca4
Nima Boscarino <nboscari@sfu.ca>
2019-06-20 18:05:46 -0700

    comment

--- rails-back-end/config/routes.rb
+++ rails-back-end/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api do
+  namespace :api do # /api/data
+
     get '/data', to: 'tests#index'
+    
+    resources :dogs
+
   end
+
 end
