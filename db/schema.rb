69bff877fae4cacc023c43384d62bee13789b34b
ZHShang <zhihengshang@hotmail.com>
2019-06-21 17:58:21 +0000

    created users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_21_172053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.text "plaid_token"
+    t.text "stripe_token"
+    t.float "balance"
+    t.date "balance_date"
+    t.integer "votes", default: [], array: true
+  end
 
 end
