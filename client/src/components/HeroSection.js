ddffe4da5cf9b62031d4a92d2c771572b98fb3cd
Kayley <kayley.luftig@gmail.com>
2019-06-28 15:37:40 +0000

    wip for display charities on home page

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -3,6 +3,7 @@ import '../HeroSection.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 const HeroSection = props => (
   <div className='App'>
@@ -51,17 +52,29 @@ const HeroSection = props => (
           </div>
         </Col>
       </Row>
-      {/*<Row className="home-charities" id="home-charities">
-        <Col>
-          <p>Hello</p>
-        </Col>
-        <Col>
-          <div>
-            <h2>Image</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in uienply voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat norin proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-        </Col>
-      </Row>*/}
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image src={'https://i.imgur.com/PdnVWGx.png'} alt="Habitat for Humanity. Habitat pour l'humanité. Canada" rounded />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image src={'https://i.imgur.com/7BkiXTN.png'} roundedCircle />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image src="holder.js/171x180" thumbnail />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={8} md={4}>
+            <Image src="holder.js/171x180" rounded />
+          </Col>
+          <Col xs={8} md={4}>
+            <Image src="holder.js/171x180" roundedCircle />
+          </Col>
+
+        </Row>
+
+      </Container>
     </Container>
   </div>
 );
