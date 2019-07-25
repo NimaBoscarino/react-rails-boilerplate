ef3caf42ff47b4578d58099916629c67ff76dc46
sdsog <34170634+sdsog@users.noreply.github.com>
2019-07-24 22:47:37 -0700

    adds pop-up carousel

---
+++ client/src/Components/Map/CurrentSelectionCard/CarouselModalSlide.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PlaceHolderPic from "../../../global-assets/yaletown.jpg";
+
+function CarouselModalSlide() {
+  return (
+    <div>
+      <div>
+        <img src={PlaceHolderPic} alt={" "} />
+      </div>
+    </div>
+  );
+}
+
+export default CarouselModalSlide;
