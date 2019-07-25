90242e8e6a1a77df754aa6f7e4cff62ac6c16c18
Nick Yu <yynickel@gmail.com>
2019-07-25 12:47:51 -0700

    added controllers for handling neighbourhoods request

---
+++ app/controllers/neighbourhoods_controller.rb
@@ -0,0 +1,27 @@
+class NeighbourhoodsController < ApplicationController
+    def index
+        nbh=Neighbourhood.all
+        array = []
+        nbh.each{|element|
+            hash=formatData(element)
+            array.push(hash)
+        }
+        render :json=>{
+            neighbourhoods: array
+        }
+    end
+
+    def formatData(neighbourhood)
+        all_border_points=neighbourhood.border_points.all
+        all_border_points_array=[]
+        all_border_points.each{|each_border_point| 
+            point={}
+            point["lat"]=each_border_point[:lat]
+            point["lng"]=each_border_point[:long]
+            all_border_points_array.push(point)
+        }
+        hash=neighbourhood.attributes
+        hash["border_points"]=all_border_points_array
+        hash
+    end
+end
