7c74d79345464ce34d728aff65c5d4c151aa36a4
sdsog <34170634+sdsog@users.noreply.github.com>
2019-07-27 19:56:07 -0700

    adds simple style

--- client/src/Components/Intro/IntroCarouselSlide.js
+++ client/src/Components/Intro/IntroCarouselSlide.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import PlaceHolderPic from "../../global-assets/yaletown.jpg";
 
@@ -10,57 +9,24 @@ function average(a, b) {
   return (a + b) / 2;
 }
 
-
-
 class IntroCarouselSlide extends Component {
-
-
-
-
   render() {
     return (
       <div className='intro-feature-card-div'>
-        <div className='card bg-dark text-white intro-feature-card shadow'>
+        <div className='card intro-feature-card'>
           <img
-            className='card-img featured-card-pic'
-            src={this.props.place.yelp_photos[0] || PlaceHolderPic} alt={""}
+            className='card-img-top'
+            src={this.props.place.yelp_photos[0] || PlaceHolderPic}
+            alt={" "}
           />
-          <div className='card-img-overlay'>
-            <div className='row m-0 p-0'>
-            
-                <h5 className='card-title'>{this.props.place.name}</h5>
-                <div className='card-text'>
-          
-                <p className="address">{this.props.place.address}</p>
-
-                <p className="type-and-price">{this.props.place.types
-              .filter(element =>
-                ["bar", "restaurant", "night_club"].includes(element)
-              )
-              .join(" / ")} | Price: {this.props.place.yelp_price}</p>
-               
-              
-
- 
-
-              <div className='hotspot-div'>
-
-              <h4 className='hotspot-title'>HotSpot Score</h4>
-
-              <div className='hotspot-number-div'>
-                <h6 className='hotspot-score'>{this.props.place.rating}%</h6>
-                <p className='total-reviews'> Total Reviews: {sum(this.props.place.rating_n, this.props.place.yelp_review_count)}</p>
-                </div>
-
-                </div>
-
-               
-               
-              
-                </div>
-            
-   
+          <div className='card-body'>
+            <div className='card-text'>
+              <p className='card-title'>
+                {this.props.place.name} {this.props.place.rating} %
+              </p>
+              <p className='hood-title'>Hottest Spot in Gastown Right Now</p>
             </div>
+
           </div>
         </div>
       </div>
