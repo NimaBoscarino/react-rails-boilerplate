cbdc7d9d51680448eb5190e26c601b821f5265dc
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 03:15:58 +0000

    the Activities component is added info from App component

---
+++ client/src/components/Activities.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function Activities(props){
+
+  const [state, setState] = useState({message:"click button to load data!"})
+
+  const fetchData = () => {
+    axios.get('/api/activities') 
+    .then((response) => {
+      console.log(response.data) // The entire response from the Rails API
+      console.log(response.data[0]) //The first activity
+      setState({
+        message: `All ${response.data.length} activities are rendered at this point`
+      });
+    }) 
+  }
+
+  return (
+    <div className="App">
+      <h1>This is the index(or home) page</h1>
+      <h1>{ state.message }</h1>
+      <button onClick={fetchData} >
+        Render all activities 
+      </button>        
+    </div>
+  )
+}
+
+export default Activities
