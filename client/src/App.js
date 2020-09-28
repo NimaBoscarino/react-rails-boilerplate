a916e77d6404773e2838b83b6ee03b9395fae056
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 03:17:14 +0000

    the App component is cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,6 @@ import Activities from './components/Activities';
 
 function App (props){
   
-//  const [state, setState] = useState({message:"click button to load data!"})
-
-//   const fetchData = () => {
-//     axios.get('/api/activities') 
-//     .then((response) => {
-//       console.log(response.data) // The entire response from the Rails API
-//       console.log(response.data[0]) //The first activity
-//       setState({
-//         message: `All ${response.data.length} activities are rendered at this point`
-//       });
-//     }) 
-//   }
-
-    // return (
-    //   <div className="App">
-    //     <h1>This is the index(or home) page</h1>
-    //     <h1>{ state.message }</h1>
-    //     <button onClick={fetchData} >
-    //       Render all activities 
-    //     </button>        
-    //   </div>
-    // );
     return(
       <Switch>
         <Route path='/' component={Activities}/>
