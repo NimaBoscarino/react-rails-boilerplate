1e0849f5897cfda5150dd71ef48d611f7bf651cb
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 04:44:28 +0000

    the Activity component is created inside component folder

---
+++ client/src/components/Activity.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Activity(props){
+  return (
+    <div className='activity'>
+      <div class='activity-img'>
+        <img src={props.item.image_url}/>
+      </div>
+      <div className='activity-title'>
+        {props.item.title}
+      </div>
+      <div className='activity-link'>
+        <a href={`/activities/${props.item.id}`}>more details</a>
+      </div>
+      <div className='activity-spot'>
+       spots remaining:{props.item.max_number_of_participants}
+      </div>
+    </div>
+  )
+}
+
+export default Activity
