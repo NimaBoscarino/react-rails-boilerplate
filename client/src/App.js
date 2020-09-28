6a72bb0c3b7e5e1d3a0e156b4b203d2857c28841
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 03:15:04 +0000

    the function fetchData and returned JSX html, useState are moved into newly created Activities.js in component folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,39 @@ import React, {  useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
+import Activities from './components/Activities';
 
 function App (props){
   
- const [state, setState] = useState({message:"click button to load data!"})
+//  const [state, setState] = useState({message:"click button to load data!"})
 
-  const fetchData = () => {
-    axios.get('/api/activities') 
-    .then((response) => {
-      console.log(response.data) // The entire response from the Rails API
-      console.log(response.data[0]) //The first activity
-      setState({
-        message: `All ${response.data.length} activities are rendered at this point`
-      });
-    }) 
-  }
+//   const fetchData = () => {
+//     axios.get('/api/activities') 
+//     .then((response) => {
+//       console.log(response.data) // The entire response from the Rails API
+//       console.log(response.data[0]) //The first activity
+//       setState({
+//         message: `All ${response.data.length} activities are rendered at this point`
+//       });
+//     }) 
+//   }
 
-    return (
-      <div className="App">
-        <h1>This is the index(or home) page</h1>
-        <h1>{ state.message }</h1>
-        <button onClick={fetchData} >
-          Render all activities 
-        </button>        
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>This is the index(or home) page</h1>
+    //     <h1>{ state.message }</h1>
+    //     <button onClick={fetchData} >
+    //       Render all activities 
+    //     </button>        
+    //   </div>
+    // );
+    return(
+      <Switch>
+        <Route path='/' component={Activities}/>
+
+        
+      </Switch>
+    )
   
 }
 
