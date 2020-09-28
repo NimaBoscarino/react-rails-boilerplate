19c1257f56a0e518b8e5ab0b2156698948333e49
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 03:59:33 +0000

    the usestate for activity are used, setActivities changes when set the activities array to response data, activityLists are use for displaying eachactivity title

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -4,17 +4,22 @@ import axios from 'axios';
 function Activities(props){
 
   const [state, setState] = useState({message:"click button to load data!"})
+  const [activities, setActivities] = useState([])
 
   const fetchData = () => {
     axios.get('/api/activities') 
     .then((response) => {
-      console.log(response.data) // The entire response from the Rails API
-      console.log(response.data[0]) //The first activity
+      console.log(response) // The entire response from the Rails API
+      setActivities(response.data) //The first activity
       setState({
         message: `All ${response.data.length} activities are rendered at this point`
       });
     }) 
   }
+
+  const activityList = activities.map(item=>{
+    return(<li key={item.id}>{item.title}</li>)
+  })
 
   return (
     <div className="App">
@@ -22,7 +27,8 @@ function Activities(props){
       <h1>{ state.message }</h1>
       <button onClick={fetchData} >
         Render all activities 
-      </button>        
+      </button> 
+      <ul>{activityList}</ul>       
     </div>
   )
 }
