9201a2727a395cd1dfbcc533a1fa88d8fbcc304c
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 18:50:56 +0000

    used useEffect and set up the id and url for api request

--- client/src/components/detail/Activity.js
+++ client/src/components/detail/Activity.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+
 
 function Activity(props){
+  const [activity, SetActivity]= useState({})
+
+  useEffect(()=>{
+    const id = props.match.params.id;
+    const url = `/api/activities/${id}`
+    console.log(props)
+
+  },[])
   return(
     <h1>hello hahahahhhah</h1>
   )
