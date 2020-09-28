a7c5670c01d3c1b46ab2907d78e402d6d65b1e25
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 02:40:07 +0000

    the App componet response are more sematics as activities are used

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,24 @@ import './App.css';
 function App (props){
   
  const [state, setState] = useState({message:"click button to load data!"})
+ 
   const fetchData = () => {
     axios.get('/api/activities') 
     .then((response) => {
-      // handle success
       console.log(response.data) // The entire response from the Rails API
       console.log(response.data[0]) //The first activity
       setState({
-        message: response.data[0].title
+        message: `All ${response.data.length} activities are rendered at this point`
       });
     }) 
   }
 
     return (
       <div className="App">
+        <h1>This is the index(or home) page</h1>
         <h1>{ state.message }</h1>
         <button onClick={fetchData} >
-          Fetch Data
+          Render all activities 
         </button>        
       </div>
     );
