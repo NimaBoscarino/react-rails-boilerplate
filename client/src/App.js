e7842eaa5b48b7d6850a651f39f5c5845c311a4e
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 01:48:21 +0000

    the class based component is replaced with functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
+import React, {  useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
-
-  fetchData = () => {
+function App (props){
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     message: 'Click the button to load data!'
+  //   }
+  // }
+ const [state, setState] = useState({message:"click button to load data!"})
+  const fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
       console.log(response.data) // The entire response from the Rails API
 
       console.log(response.data.message) // Just the message
-      this.setState({
+      setState({
         message: response.data.message
       });
     }) 
   }
 
-  render() {
+  
     return (
       <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
+        <h1>{ state.message }</h1>
+        <button onClick={fetchData} >
           Fetch Data
         </button>        
       </div>
     );
-  }
+  
 }
 
 export default App;
