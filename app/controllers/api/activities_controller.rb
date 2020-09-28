d965aae9426f958faf722dcbb8771c39296a7236
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 19:05:42 +0000

    the show method is changed by replacing find_by with find

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -5,7 +5,7 @@ class Api::ActivitiesController < ApplicationController
   end
 
   def show
-    @activity = Activity.find_by(params[:id])
+    @activity = Activity.find(params[:id])
     render json: @activity
   end
 end
