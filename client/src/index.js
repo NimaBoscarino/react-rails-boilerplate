082f5efc98444379dac7012f23132d771dbc3cf8
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-28 02:53:48 +0000

    the react-router-dom is installed

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import {BrowserRouter as Router, Route}  from "react-router-dom"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
