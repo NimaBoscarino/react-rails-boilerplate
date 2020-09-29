25696107f10c5bb546f76aaa0426b3498504a188
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-09-29 18:34:01 +0000

    each dropdowm.item is added with setCity function

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -4,7 +4,7 @@ import Activity from "./Activity"
 import "../css/activities.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap'
-import {UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'react-bootstrap'
+import {Dropdown} from 'react-bootstrap'
 
 function Activities(props) {
 
@@ -22,7 +22,7 @@ function Activities(props) {
         });
       })
   }
-
+  console.log(city)
   const activityList = activities.map(item => {
     return (
       <Activity
@@ -34,6 +34,20 @@ function Activities(props) {
 
   return (
     <Fragment>
+     <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+        {city}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={()=>{setCity('Vancouver')}}>Vancouver</Dropdown.Item>
+          <Dropdown.Item> onClick={()=>{setCity('North Vancouver')}}North Vancouver</Dropdown.Item>
+          <Dropdown.Item onClick={()=>{setCity('Whistler')}}>Whistler</Dropdown.Item>
+          <Dropdown.Item onClick={()=>{setCity('Delta')}}>Delta</Dropdown.Item>
+          <Dropdown.Item onClick={()=>{setCity('Victoria')}}>Victoria</Dropdown.Item>
+          <Dropdown.Item onClick={()=>{setCity('Squamish')}}>Squamish</Dropdown.Item>
+        </Dropdown.Menu>
+     </Dropdown>
       <div className="App">
         <h1>{state.message}</h1>
         <Button outline color='primary' onClick={fetchData} >
