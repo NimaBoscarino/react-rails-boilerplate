e960eb680f5e03d83d8e54f01ed65d1b96b9307a
Caia Chuang <caiachuang@gmail.com>
2020-09-29 14:54:28 -0700

    Modify relations between users and activities in activity model and activity serializer.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
-  has_many :users, through: :bookings
+  #has_many :users, through: :bookings
   has_many :bookings, dependent: :destroy
   has_many :favorites, dependent: :destroy
 end
