d965c01fbaf6ab37ddf4930e5af3232b64f39b6d
Caia Chuang <caiachuang@gmail.com>
2020-09-29 09:08:59 -0700

    Modify bookings component and controller so that data can be sent to the FE.

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,22 +1,29 @@
 class Api::BookingsController < ApplicationController
+  def index
+    bookings = Booking.all
+    render json: bookings
+  end
+
   def show
-    booking = Booking.find(params[:user_id])
+    bookings = Booking.all
+    render json: bookings
+    # booking = Booking.find(params[:user_id])
   end
 
-  def create
-    activity = Activity.finde_by(id: params[:activity_id])
+  # def create
+  #   activity = Activity.finde_by(id: params[:activity_id])
 
-    booking = Booking.new(
-      number_of_participants: params[:number_of_participants]
-      price_per_person: activity.price_per_person 
+  #   booking = Booking.new(
+  #     number_of_participants: params[:number_of_participants]
+  #     price_per_person: activity.price_per_person 
 
-    )
-  end
+  #   )
+  # end
   
-  def destroy
-    booking = Booking.find(params[:id])
-    booking.delete
-  end
+  # def destroy
+  #   booking = Booking.find(params[:id])
+  #   booking.delete
+  # end
 
 
 end
--- client/src/components/Bookings.js
+++ client/src/components/Bookings.js
@@ -4,45 +4,37 @@ import axios from 'axios';
 import { Table } from 'react-bootstrap';
 
 export default function Bookings() {
-  // const [bookings, setBookings] = useState([]);
+  const [bookings, setBookings] = useState([]);
 
-  // useEffect(() => {
-  //   axios.get('/api/bookings')
-  //   .then(res => setBookings(res.data))
-  //   .catch(err => console.log(err.message))
-  // }, [bookings.length])
-
+  useEffect(() => {
+    axios.get('/api/bookings')
+    .then(res => {
+      setBookings(res.data)})
+    .catch(err => console.log("bookings.js err: ", err))
+  }, [bookings.length])
+  
+  const bookingItems = bookings.map(booking => {
+    return (
+      <tr  key={booking.id}>
+        <td>{booking.user_id}</td>
+        <td>{booking.activity_id}</td>
+      </tr>
+    )
+  })
   return (
     <>
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Activity</th>
-          <th>Spots Available</th>
-          <th>Date</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
+    <h2>Bookings: </h2>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>User_id</th>
+            <th>Activity_id</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookingItems}
+        </tbody>
+      </Table>
     </>
   )
 }
