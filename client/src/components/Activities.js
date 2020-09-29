163ecb2374a05b0b2b25a7d2040052f808068dca
Jenny Citherlet <jennycith@gmail.com>
2020-09-29 03:10:49 +0000

    Fix typo

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -1,48 +1,48 @@
-import React, {Fragment, useState} from 'react';
+import React, { Fragment, useState } from 'react';
 import axios from 'axios';
 import Activity from "./Activity"
 import "../css/activities.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import { Button } from 'react-bootstrap'
 
-function Activities(props){
+function Activities(props) {
 
-  const [state, setState] = useState({message:"Find a activity nearby!"})
+  const [state, setState] = useState({ message: "Find a activity nearby!" })
   const [activities, setActivities] = useState([])
 
   const fetchData = () => {
-    axios.get('/api/activities') 
-    .then((response) => {
-      console.log(response) // The entire response from the Rails API
-      setActivities(response.data) //The first activity
-      setState({
-        message: `All ${response.data.length} activities are rendered at this point`
-      });
-    }) 
+    axios.get('/api/activities')
+      .then((response) => {
+        console.log(response) // The entire response from the Rails API
+        setActivities(response.data) //The first activity
+        setState({
+          message: `All ${response.data.length} activities are rendered at this point`
+        });
+      })
   }
 
-  const activityList = activities.map(item=>{
-    return(
-    <Activity 
-      key={item.id}
-      item={item} 
-    />
+  const activityList = activities.map(item => {
+    return (
+      <Activity
+        key={item.id}
+        item={item}
+      />
     )
   })
 
   return (
     <Fragment>
-    <div className="App">
-      <h1>This is the index(or home) page of our App</h1>
-      <h1>{ state.message }</h1>
-      <Button outline color='primary' onClick={fetchData} >
-        Find Activities nearby 
-      </Button>       
-    </div>
-    
+      <div className="App">
+        <h1>This is the index(or home) page of our App</h1>
+        <h1>{state.message}</h1>
+        <Button outline color='primary' onClick={fetchData} >
+          Find Activities nearby
+      </Button>
+      </div>
+
       <ul className='grid'>{activityList}</ul>
-    
-  </Fragment>
+
+    </Fragment>
   )
 }
 
