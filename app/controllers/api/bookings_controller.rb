18392fe0541ab324cd3ef96d9c4379689360a5d4
Caia Chuang <caiachuang@gmail.com>
2020-09-30 14:19:25 -0700

    Modify bookings methods to access bookings through user_id.

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,29 +1,30 @@
 class Api::BookingsController < ApplicationController
   def index
-    bookings = Booking.all
+    # bookings = Booking.all
+    bookings = Booking.where(user_id: 1)
     render json: bookings
     # bookings = Booking.all.includes(:activities)
     # render json: bookings, include: ['acritities']
   end
 
   def show
-    booking = Booking.find_by(user_id: 1)
+    booking = Booking.where(user_id: 1)
     render json: booking
     # render json: BookingSerializer.new(booking, @options).serial
     # render json: booking, include: ["activity", 'user']
   end
 
-  def create
-    activity = Activity.finde_by(id: params[:activity_id])
+  # def create
+  #   activity = Activity.finde_by(id: params[:activity_id])
 
-    booking = Booking.new(
-      number_of_participants: params[:number_of_participants]
-      price_per_person: activity.price_per_person 
-    )
-  end
+  #   booking = Booking.new(
+  #     number_of_participants: params[:number_of_participants]
+  #     price_per_person: activity.price_per_person 
+  #   )
+  # end
   
-  def destroy
-    booking = Booking.find(params[:id])
-    booking.delete
-  end
+  # def destroy
+  #   booking = Booking.find(params[:id])
+  #   booking.delete
+  # end
 end
