bb3f932116123d1322ff3f50c6c94bf1d1d1d947
Caia Chuang <caiachuang@gmail.com>
2020-09-30 14:18:28 -0700

    Create user_activities method in activities controller to access activity details from user_id.

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -8,4 +8,9 @@ class Api::ActivitiesController < ApplicationController
     @activity = Activity.find(params[:id])
     render json: @activity
   end
+
+  def user_activities
+    activities = Activity.joins(:bookings).where(bookings: {user_id: 1})
+    render json: activities
+  end
 end
