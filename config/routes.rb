4d5bb8c95cb0afe16645dbb9cdc58bf754b4680a
Caia Chuang <caiachuang@gmail.com>
2020-09-30 14:20:39 -0700

    Create a route for accessing activities through user_id.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  # get '/api/users/:user_id/bookings'
+
   namespace :api do # /api/data
 
+    get '/activities/user/:user_id', to: 'activities#user_activities'
     # get '/data', to: 'tests#index'
-    resources :activities
-    resources :users, param: :id 
-    resources :bookings#, only: [:show, :create, :destroy]
-    resources :favorites#, only: [:show, :create, :destroy]
+    resources :activities 
+
+
+    resources :users do
+      resources :bookings#, controller: 'api/bookings'#, only: [:show, :create, :destroy]
+      resources :favorites#, only: [:show, :create, :destroy]
+    end
 
   end
 
