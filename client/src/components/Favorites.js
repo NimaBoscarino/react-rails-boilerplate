f95ee1977969a3a9efa3f6375fba936212dc0b69
Jenny Citherlet <jennycith@gmail.com>
2020-10-01 20:09:05 +0000

    Add container to favorites box

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import "../css/favorites.css"
 
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, Container } from 'react-bootstrap';
 
 export default function Favorites() {
   const [favorites, setFavorites] = useState([]);
@@ -12,17 +13,18 @@ export default function Favorites() {
       Promise.resolve(axios.get('/api/users/1/favorites')),
       Promise.resolve(axios.get('/api/activities/user/1'))
     ])
-    .then(all => {
-      console.log(all)
-      setFavorites(all[0].data)
-      setActivities(all[1].data)
-    })
-    .catch(err => console.log("favorites.js err: ", err))
+      .then(all => {
+        console.log(all)
+        setFavorites(all[0].data)
+        setActivities(all[1].data)
+      })
+      .catch(err => console.log("favorites.js err: ", err))
   }, [])
-  
+
   const favoredItems = activities.map(activity => {
     return (
-      <tr  key={activity.id}>
+
+      <tr key={activity.id}>
         <td>{activity.title}</td>
         <td>status</td>
         <td>{activity.max_number_of_participants}</td>
@@ -31,29 +33,32 @@ export default function Favorites() {
           <Button variant="success">Join</Button>
         </td>
         <td>
-          <Button variant="danger">Delete</Button> 
+          <Button variant="danger">Delete</Button>
         </td>
       </tr>
+
     )
   })
   return (
     <>
-    <h2>Favorites: </h2>
-      <Table striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>Activity Title</th>
-            <th>Status</th>
-            <th>Spots Available</th>
-            <th>Date</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {favoredItems}
-        </tbody>
-      </Table>
+      <Container className="favorite-box">
+        <h1>My Favorites </h1>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Activity Title</th>
+              <th>Status</th>
+              <th>Spots Available</th>
+              <th>Date</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {favoredItems}
+          </tbody>
+        </Table>
+      </Container>
     </>
   )
 }
