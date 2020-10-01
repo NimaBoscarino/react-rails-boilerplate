bc166d2dfd6b5846a87530d4068bf67a7a71c4f6
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-10-01 18:54:30 +0000

    the useState is used for price and setPrice function to change price according to spots selected

--- client/src/components/confirmation.js
+++ client/src/components/confirmation.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function Confirmation(props){
-  console.log(props)
-  return <h1>hello?</h1>
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState(0)
+
+  useEffect(() => {
+    const id = props.match.params.id;
+    const url = `/api/activities/${id}`
+    axios.get(url)
+      .then(
+        res => {setTitle(res.data.title)
+        console.log(res)
+        })
+        
+      .catch(res => console.log(res))
+
+  }, [])
+  return(
+    <>
+    <h1>Awesome, let's have you booked in for</h1>
+    <h2>{title}</h2>
+    <h3>How many spots you want to book?</h3>
+    <input type ='number' 
+    min='1'
+    placeholder='select your spots'
+    onChange={(event) => setPrice(event.target.value)}
+    />
+
+    
+    <h3>The total price is ${price}</h3>
+    </>
+  )
 
 }
 
