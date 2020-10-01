9f3dfee18ca7b060aff32505f8dfade606f67973
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-10-01 19:10:39 +0000

    the functionality of calculating total priceis implemented

--- client/src/components/confirmation.js
+++ client/src/components/confirmation.js
@@ -4,19 +4,23 @@ import axios from 'axios'
 function Confirmation(props){
   const [title, setTitle] = useState('')
   const [price, setPrice] = useState(0)
+  const [individualPrice, setIndividualPrice] = useState(0)
 
   useEffect(() => {
     const id = props.match.params.id;
     const url = `/api/activities/${id}`
     axios.get(url)
-      .then(
-        res => {setTitle(res.data.title)
+      .then(res => 
+        {setTitle(res.data.title)
         console.log(res)
+        setIndividualPrice(res.data.price_per_person)
+        console.log(typeof price_per_person)
         })
         
       .catch(res => console.log(res))
 
   }, [])
+  
   return(
     <>
     <h1>Awesome, let's have you booked in for</h1>
@@ -24,12 +28,12 @@ function Confirmation(props){
     <h3>How many spots you want to book?</h3>
     <input type ='number' 
     min='1'
-    placeholder='select your spots'
-    onChange={(event) => setPrice(event.target.value)}
+    placeholder='enter your spots'
+    onChange={event => 
+      setPrice(event.target.value * individualPrice)}
     />
-
     
-    <h3>The total price is ${price}</h3>
+    <h3>The total price is $ {price}</h3>
     </>
   )
 
