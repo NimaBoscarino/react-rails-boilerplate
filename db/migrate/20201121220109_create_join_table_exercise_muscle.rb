6d79ff340c1ce89e6188e0b3c099fc9d22d3eba4
Bryn <bschulha@ualberta.ca>
2020-11-23 23:08:41 +0000

    Switch to local db

--- db/migrate/20201121220109_create_join_table_exercise_muscle.rb
+++ db/migrate/20201121220109_create_join_table_exercise_muscle.rb
@@ -5,5 +5,7 @@ class CreateJoinTableExerciseMuscle < ActiveRecord::Migration[5.2]
       # t.index [:muscle_id, :exercise_id]
     end
     rename_table :workout_sessions, :workouts
+    rename_table :user_exercises_for_sessions, :user_exercises_for_workout
+    drop_table :artists_musics
   end
 end
