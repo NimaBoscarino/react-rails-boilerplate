1b6ec9eafb5bb9e675d4e599260dee9713aeef12
vvEii <coopershang125@gmail.com>
2020-11-23 20:36:54 -0800

    Change newSession to newWorkout

--- client/src/components/NewWorkout/index.tsx
+++ client/src/components/NewWorkout/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ExerciseList } from './ExerciseList';
 
-export const NewSession = () => {
+export const NewWorkout = () => {
   return(
     <ExerciseList/>
   )
