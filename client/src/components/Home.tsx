f4bf85cb636e9e37dafbf75c538fbe3221e1a7a0
Bryn <bschulha@ualberta.ca>
2020-11-30 19:53:33 +0000

    Progress circles on home page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Progress } from 'antd';
+
+
 export const Home = ():React.ReactElement => {
   return(
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Progress type="circle" percent={30} width={80} />
+      <Progress type="circle" percent={70} width={80} status="exception" />
+      <Progress type="circle" percent={100} width={80} />
+    </>
   )
 }
