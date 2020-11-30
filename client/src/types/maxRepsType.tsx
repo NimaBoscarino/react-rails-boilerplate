2d23fb48d63113e5f6aaee42ee9852e2012225f8
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-30 23:25:47 +0000

    updated types for use in home and with insight call

--- client/src/types/maxRepsType.tsx
+++ client/src/types/maxRepsType.tsx
@@ -1,1 +1,19 @@
-a 
+export interface IMax {
+  // with how we are currently importing 
+  //api call for insights, shortcut
+  //reusing same structure for hardcoded goals
+  "3":number,
+  "4":number,
+  "6":number,
+  date:string
+}
+
+export interface IDay {
+  workout_date:string,
+  sum:number
+}
+
+export interface State {
+  alldays:IDay[],
+  currentProgress:IMax[]
+}
