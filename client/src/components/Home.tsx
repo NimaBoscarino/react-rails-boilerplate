8ebebcd1bb297dfe09c5900981f6906064a123b1
Bryn <bschulha@ualberta.ca>
2020-12-01 02:43:44 +0000

    Have the goals updated on the home page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHomeData } from '../hooks/useHomeData';
 import { Progress } from 'antd';
 import {IDay, IMax, State} from '../types/maxRepsType';
-
+//color code chagnes weeklyc ircle color based on total reps by day
 const colorCode = (sum:number):"success"|"normal"|"exception"|"active" => {
   let status:"success"|"normal"|"exception"|"active" = "success";
   if (sum > 80) {
@@ -15,33 +15,105 @@ const colorCode = (sum:number):"success"|"normal"|"exception"|"active" => {
   }
 return status;
 }
-
+// cuts off year from date 
 const sliceDate = (date:string):string => {
   let result = date.split("").slice(5, 10).join("");
   return result;
 }
-
-const getPercentMax = (max:number):number => {
-  
+//getting highest, not current max squat
+const getOneRepMaxSquat = (currentProgress:IMax[]) => {
+  let oneRepMax = 0;
+  currentProgress.forEach((element, index, array) => {
+    if (element["3"] > oneRepMax) {
+      oneRepMax = element["3"];
+    }
+  })
+  return oneRepMax;
+}
+//same for RDL
+const getOneRepMaxRDL = (currentProgress:IMax[]) => {
+  let oneRepMax = 0;
+  currentProgress.forEach((element, index, array) => {
+    if (element["4"] > oneRepMax) {
+      oneRepMax = element["4"];
+    }
+  })
+  return oneRepMax;
+}
+//dame for bench, currently hardcoded, wil be updated
+const getOneRepMaxBench = (currentProgress:IMax[]) => {
+  let oneRepMax = 0;
+  currentProgress.forEach((element, index, array) => {
+    if (element["6"] > oneRepMax) {
+      oneRepMax = element["6"];
+    }
+  })
+  return oneRepMax;
+}
+// returns current three lift sum
+const thousandPoundClub = (num1:number, num2:number, num3:number) => {
+  let result = num1 + num2 + num3;
+  return result;
+}
+//potential area of refactor, push max reps of goal to array, then map over arraywith function, great for MVD
+// next four convert max rep to pecentage based on hard coded goal, once we are able to input goals will modify accordingly
+const bpConverter = (num:number) => {
+  let result = Math.round(num / 275 * 100);
+  return result;
 }
 
+const bsConverter = (num:number) => {
+  let result = Math.round(num / 315 * 100);
+  return result;
+}
+
+const rdlConverter = (num:number) => {
+  let result = Math.round(num / 410 * 100);
+  return result;
+}
+
+const tPCConverter = (num:number) => {
+  let result = Math.round(num / 10);
+  return result;
+}
+
+
+
+
 export const Home = ():React.ReactElement => {
-  const weeklyWorkouts = useHomeData();
-  console.log(weeklyWorkouts);
+  const {allDays, currentProgress} = useHomeData();
+  console.log(allDays);
+  console.log(currentProgress);
 
   return(
     <>
     <h1>This Weeks Gains</h1>
     <div>
-      {weeklyWorkouts.map((day:any) => (
+      {allDays.map((day:any) => (
       <Progress type="circle" percent={day["sum"]} width={50} status={colorCode(day["sum"])} format={() => `${sliceDate(day.workout_date)}`} />
       ))}
       </div>
-      {/* <div>
-      {placeholder.map((day:any) => (
-      <Progress percent={50} status={colorCode(day["sum"])}  />
-      ))}
-      </div> */}
+      { <div>
+      {/* all goals currely hardcoded */}
+      <h3>Bench Press Goal: 275</h3>
+      <p>Currently lifting: {getOneRepMaxBench(currentProgress)}</p>
+      <Progress percent={bpConverter(getOneRepMaxBench(currentProgress))} status="active" />
+      
+      
+      <h3>Back Squat Goal: 315</h3>
+      <p>Currently lifting: {getOneRepMaxSquat(currentProgress)}</p>
+      <Progress percent={bsConverter(getOneRepMaxSquat(currentProgress))} status="active" /> 
+      
+      
+      <h3>RDL Goal: 410</h3>
+      <p>Currently lifting: {getOneRepMaxRDL(currentProgress)}</p>
+      <Progress percent={rdlConverter(getOneRepMaxRDL(currentProgress))} status="active" />
+      
+      
+      <h3>1000 lb Club</h3>
+      <p>Currently lifting: {thousandPoundClub(getOneRepMaxBench(currentProgress),getOneRepMaxSquat(currentProgress),getOneRepMaxRDL(currentProgress))} </p>
+      <Progress percent={tPCConverter(thousandPoundClub(getOneRepMaxBench(currentProgress),getOneRepMaxSquat(currentProgress),getOneRepMaxRDL(currentProgress)))} status="active" /> 
+      </div> }
     </>
   )
 }
