12870476b162611bc042715fc2e6e65c7613dd3a
Pegasusor001 <linus.ji.001@gmail.com>
2021-08-04 16:58:09 -0700

    add route, start styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+
+// import FilterBar from "./componentsArtist/FilterBar.jsx"
+// import Button from "./componentsArtist/Button.jsx"
+// import UserGallery from "./componentsUser/UserGallery.jsx"
+// import Payment from "./componentsUser/Payment.jsx"
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
-
-
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import ArtistRequests from "./componentsArtist/ArtistRequests.jsx"
-import ArtistAccount from "./componentsArtist/ArtistAccount.jsx"
-import FilterBar from "./componentsArtist/FilterBar.jsx"
-import Button from "./componentsArtist/Button.jsx"
-
-import UserGallery from "./componentsUser/UserGallery.jsx"
-import Payment from "./componentsUser/Payment.jsx"
-import UserCommission from "./componentsUser/UserCommission"
-import UserAccount from "./componentsUser/UserAccount.jsx"
-
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Button />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister(); */
