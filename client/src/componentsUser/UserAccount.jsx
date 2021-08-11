12df3b078e22fb01e36438767b5017d341a84d3e
Senayson <senaialema@gmail.com>
2021-08-11 21:35:29 +0000

    Update User Account

--- client/src/componentsUser/UserAccount.jsx
+++ client/src/componentsUser/UserAccount.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useContext, useEffect} from "react";
 import axios from 'axios';
+import '../componentsArtist/ArtistAccount.css'
 import Cookies from 'universal-cookie';
-
 import useData from "../hooks/useData.js";
 import DashboardEditUser from "../componentsUser/DashboardEditUser"
 import DashboardShowUser from "../componentsUser/DashboardShowUser"
