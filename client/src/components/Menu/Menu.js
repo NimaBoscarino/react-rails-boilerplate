133d479879c1fe9fb5cf7f5cc0045f0d7b456321
Pegasusor001 <linus.ji.001@gmail.com>
2021-08-11 14:26:14 -0700

    add style

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -11,43 +11,6 @@ const Menu = ({ open, ...props }) => {
 
   const isHidden = open ? true : false;
   const tabIndex = isHidden ? 0 : -1;
-
-<<<<<<< HEAD
-  return (
-    
-    <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
-      <a href="/" tabIndex={tabIndex}>
-        <span aria-hidden="true"> </span>
-        Home
-      </a>
-      <a href="/about" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
-        About us
-      </a>
-      <a href="/login" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
-        Login
-      </a>
-      <a href="/register" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
-        Register
-      </a>
-      <a href="/gallery" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
-        Gallery
-        
-      </a>
-      <a href="/ArtistRequests" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
-        Commission Requests
-      </a>
-      <a href="/ArtistAccount" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
-        My Artist Account
-      </a>
-    </StyledMenu>
-  );
-=======
   const cookies = new Cookies();
 
   const user_id = cookies.get('user_id')
@@ -146,7 +109,6 @@ const Menu = ({ open, ...props }) => {
   }
 
 
->>>>>>> master
 };
 
 Menu.propTypes = {
