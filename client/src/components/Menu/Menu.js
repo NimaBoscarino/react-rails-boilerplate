e346d74921252aa0a2cc781d691c83e3ceb1df36
Senayson <senaialema@gmail.com>
2021-08-11 20:25:00 +0000

    Align UserCommission form

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,60 +1,70 @@
 import React, { useState } from "react";
 import { bool } from "prop-types";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 import { StyledMenu } from "./Menu.styled";
-import "../layout.css"
-const {requests_for_test, artists_for_test, users_for_test} = require("../../testingData")
-const {getRequestsbyArtists, getFinishedRequests, getUnFinishedRequests, getRequestsbyCategory,getRequestsbyUser, findUserbyUserId, getRequestsbyStatus, findArtistbyUserId, findArtistbyArtistId} = require("../../helpers/selectors")
+import "../layout.css";
+const {
+  requests_for_test,
+  artists_for_test,
+  users_for_test,
+} = require("../../testingData");
+const {
+  getRequestsbyArtists,
+  getFinishedRequests,
+  getUnFinishedRequests,
+  getRequestsbyCategory,
+  getRequestsbyUser,
+  findUserbyUserId,
+  getRequestsbyStatus,
+  findArtistbyUserId,
+  findArtistbyArtistId,
+} = require("../../helpers/selectors");
 
 const Menu = ({ open, ...props }) => {
-  const [display, setdisplay] = useState("xxx")
+  const [display, setdisplay] = useState("xxx");
 
   const isHidden = open ? true : false;
   const tabIndex = isHidden ? 0 : -1;
 
-<<<<<<< HEAD
   return (
-    
     <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
       <a href="/" tabIndex={tabIndex}>
         <span aria-hidden="true"> </span>
         Home
       </a>
       <a href="/about" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
+        <span aria-hidden="true"> </span>
         About us
       </a>
       <a href="/login" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
+        <span aria-hidden="true"> </span>
         Login
       </a>
       <a href="/register" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
+        <span aria-hidden="true"> </span>
         Register
       </a>
       <a href="/gallery" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
+        <span aria-hidden="true"> </span>
         Gallery
-        
       </a>
       <a href="/ArtistRequests" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
+        <span aria-hidden="true"> </span>
         Commission Requests
       </a>
       <a href="/ArtistAccount" tabIndex={tabIndex}>
-        <span aria-hidden="true">  </span>
+        <span aria-hidden="true"> </span>
         My Artist Account
       </a>
     </StyledMenu>
   );
-=======
   const cookies = new Cookies();
 
-  const user_id = cookies.get('user_id')
-  const user_identity = cookies.get('identity')
+  const user_id = cookies.get("user_id");
+  const user_identity = cookies.get("identity");
 
   if (user_id && user_identity === "client") {
-    const client = findUserbyUserId(users_for_test, user_id)[0]
+    const client = findUserbyUserId(users_for_test, user_id)[0];
     return (
       <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
         <a href="/" tabIndex={tabIndex}>
@@ -74,20 +84,23 @@ const Menu = ({ open, ...props }) => {
           Submit your Request
         </a>
         <a href="/UserAccount" tabIndex={tabIndex}>
-          <span aria-hidden="true"> 🗂  </span>
+          <span aria-hidden="true"> 🗂 </span>
           Your Account
         </a>
-        <button 
+        <button
           className="logout"
           onClick={() => {
-          cookies.set('user_id', "", { path: '/' });
-          cookies.set('identity', '', { path: '/' });
-          setdisplay("logout")
-        }}>Logout {client.name} </button>
+            cookies.set("user_id", "", { path: "/" });
+            cookies.set("identity", "", { path: "/" });
+            setdisplay("logout");
+          }}
+        >
+          Logout {client.name}{" "}
+        </button>
       </StyledMenu>
     );
-  } else if  (user_id && user_identity === "artist") {
-    const artist = findArtistbyArtistId(artists_for_test, user_id)[0]
+  } else if (user_id && user_identity === "artist") {
+    const artist = findArtistbyArtistId(artists_for_test, user_id)[0];
     return (
       <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
         <a href="/" tabIndex={tabIndex}>
@@ -107,15 +120,19 @@ const Menu = ({ open, ...props }) => {
           Requests
         </a>
         <a href="/ArtistAccount" tabIndex={tabIndex}>
-          <span aria-hidden="true"> 🗂  </span>
+          <span aria-hidden="true"> 🗂 </span>
           Your Account
         </a>
-        <button className="logout" onClick={() => {
-          cookies.set('user_id', "", { path: '/' });
-          cookies.set('identity', '', { path: '/' });
-          setdisplay("logout")
-        }}>Logout! {artist.name}</button>
-
+        <button
+          className="logout"
+          onClick={() => {
+            cookies.set("user_id", "", { path: "/" });
+            cookies.set("identity", "", { path: "/" });
+            setdisplay("logout");
+          }}
+        >
+          Logout! {artist.name}
+        </button>
       </StyledMenu>
     );
   } else {
@@ -144,9 +161,6 @@ const Menu = ({ open, ...props }) => {
       </StyledMenu>
     );
   }
-
-
->>>>>>> master
 };
 
 Menu.propTypes = {
--- client/src/componentsUser/UserCommission.jsx
+++ client/src/componentsUser/UserCommission.jsx
@@ -5,6 +5,7 @@ import DatePicker from "react-datepicker";
 import "./UserCommission.css"
 import "react-datepicker/dist/react-datepicker.css";
 import Cookies from 'universal-cookie';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // set up cookies, and request cookies 
 const cookies = new Cookies();
@@ -42,38 +43,97 @@ export default function DashboardEditArtist(props) {
   }
   
   return (
-    <article className="commission_article">
+    <div className="col-lg-6 col-md-4  commission_article">
       <form className="commision_user_form">
-        <label htmlFor="user_commision_name">Name of request: </label>
-        <input name="user_commision_name" value={request.name} id="user_commision_name" onChange={(event) => updateContent(event.target.value, "name")}/>
+        <label htmlFor="user_commision_name">REQUEST TITLE: </label>
+        <input className="form-control" name="user_commision_name" value={request.name} id="user_commision_name" onChange={(event) => updateContent(event.target.value, "name")}/>
 
-        <label htmlFor="user_commision_expected_finish_date">Expect to finish by: </label>
-        <DatePicker id="user_commision_expected_finish_date" selected={startDate} onChange={(date) => 
+      <br/>
+
+        <label htmlFor="user_commision_expected_finish_date">EXPECTED FINISH DATE </label>
+        <DatePicker className="form-control" id="user_commision_expected_finish_date" selected={startDate} onChange={(date) => 
           {setStartDate(date);
           updateContent(date.toDateString().slice(4), "expected_finish_date")}} />
 
-        <label htmlFor="user_commision_price">Budget ($): </label>
-        <input name="price" value={request.price} id="user_commision_price" type="number" onChange={(event) => updateContent(event.target.value, "price")}/>
+      <br/>
+
+        <label htmlFor="user_commision_price"> BUDGET ($): </label>
+        <input className="form-control" name="price" value={request.price} id="user_commision_price" type="number" onChange={(event) => updateContent(event.target.value, "price")}/>
         
-        <label htmlFor="user_commision_imageURL">Support Img: </label>
-        <input name="image URL" value={request.image} id="user_commision_imageURL" onChange={(event) => updateContent(event.target.value, "image")}/>
+      <br/>
 
-        <label htmlFor="user_commision_description">Description: </label>
-        <input name="description" value={request.description} id="user_commision_description" onChange={(event) => updateContent(event.target.value, "description")} />
+        <label htmlFor="user_commision_imageURL">SUPPORTING IMAGE: </label>
+        <input className="form-control" name="image URL" value={request.image} id="user_commision_imageURL" onChange={(event) => updateContent(event.target.value, "image")}/>
 
-        <label for="user_commision_category">Category:</label>
-        <select id="user_commision_category" name="user_commision_category" onChange={(event) => updateContent(event.target.value, "ategory_id")}>
+        <br/>  
+
+        <label htmlFor="user_commision_description">DESCRIPTION: </label>
+        <textarea maxlength="100" className="form-control" name="description" value={request.description} id="user_commision_description" onChange={(event) => updateContent(event.target.value, "description")} />
+        <br/>
+        <label for="user_commision_category">CATEGORY:</label>
+        <select className="form-control" id="user_commision_category" name="user_commision_category" onChange={(event) => updateContent(event.target.value, "ategory_id")}>
           <option value="1">Guitar</option>
           <option value="2">Art</option>
           <option value="3">HandyCraft</option>
         </select>
+        <hr/>
+      <footer className="commission_show_footer">
+        <Button  onClick={submitRequest} name="SUBMIT"/>
+      </footer>
       </form>
 
-      <footer className="commission_show_footer">
-        <Button onClick={submitRequest} name="submit"/>
-      </footer>
-    </article>
+    </div>
   )
+
+  // return (
+    
+  //       <div id="new-listing" className="col-lg-9 col-md-6 ">
+  //       <div class="card ml-1 mr-1 col-lg-6 col-md-4" id="main-field">
+    
+    
+  //         <form id="listing-form">
+  //           <label><h4>CREATE NEW LISTING</h4></label>
+  //           <div class="form-group">
+  //             <label for="Title">Title</label>
+  //             <input class="form-control" id="Title" name="title" aria-describedby="emailHelp" placeholder="Title" />
+  //           </div>
+  //           <div class="form-group">
+  //             <label for="listingDescription">Description</label>
+  //             <textarea class="form-control" id="Description" maxlength="100" name="description" placeholder="Description"></textarea>
+  //           </div>
+    
+  //           <div class="form-group">
+  //             <label for="price">Price</label>
+  //             <input type="number" class="form-control" id="listingPrice" name="price" placeholder="Price" />
+  //           </div>
+  //           <div class="form-group">
+  //             <label for="quantitiy">Quantity</label>
+  //             <input type="number" class="form-control" id="listingQuantitiy" name="quantity" placeholder="Quantity" />
+  //           </div>
+  //           <div class="form-group">
+  //             <label for="listingPicture">Item picture</label>
+  //             <input type="url" class="form-control" id="listingPicture" name="cover_picture_url" placeholder="Item picture" />
+  //           </div>
+  //           <div class="form-group">
+  //             <label for="exampleFormControlSelect1">Category</label>
+  //             <select class="form-control" id="categorySelect" name="category_id">
+  //               <option value='1'>Fiction</option>
+  //               <option value='2'>Non-fiction</option>
+  //               <option value='3'>Sci-fi</option>
+  //               <option value='4'>Romance</option>
+  //               <option value='5'>Economics</option>
+  //               <option value='5'>History</option>
+  //               <option value='5'>Self-Help</option>
+  //               <option value='5'>Tech</option>
+  //               <option value='5'>Religion</option>
+  //             </select>
+  //           </div>
+    
+  //         <button type="submit" class="btn btn-primary">Create Listing</button>
+  //       </form>
+  //     </div>
+  //       </div>   
+  //   )
 }
 
 
