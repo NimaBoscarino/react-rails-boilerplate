0b85b4e1623f43760f19eb97a1c85db5ffb2f83a
Pegasusor001 <linus.ji.001@gmail.com>
2021-08-10 17:36:54 -0700

    connect with backend on artist request page

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,11 +4,11 @@ class Artist < ApplicationRecord
   has_many :requests
   has_many :messages
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: {case_sensitive: false}
-  validates :phone_number, presence: true
-  validates :password, presence: true, length: { in: 8..20}
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, presence: true, uniqueness: {case_sensitive: false}
+  # validates :phone_number, presence: true
+  # validates :password, presence: true, length: { in: 8..20}
   # validates :password_confirmation, presence: true
   before_validation :remove_whitespace, :only => [:email]
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require_relative '../lib/populator_fix.rb'
-
-
-
+# rake db:reset 
+# rake db:seed
+# bin/rails c
+# Client.count 
+# Client.all
 
 puts "Starting seeds..."
 
-puts "Creating categories..."
-cat1 = Category.find_or_create_by! name: 'Painting'
-cat2 = Category.find_or_create_by! name: 'Sculpture'
-cat3 = Category.find_or_create_by! name: 'Illustration'
+# puts "Creating categories..."
+# cat1 = Category.find_or_create_by! name: 'Painting'
+# cat2 = Category.find_or_create_by! name: 'Sculpture'
+# cat3 = Category.find_or_create_by! name: 'Illustration'
 
 ArtistCategory.destroy_all
 Comment.destroy_all
@@ -23,78 +25,288 @@ Review.destroy_all
 Request.destroy_all
 Client.destroy_all
 Artist.destroy_all
+Category.destroy_all
 
-puts "Creating Clients..."
-Client.populate 6 do |c|
-  c.first_name = Faker::Name.first_name
-   c.last_name = Faker::Name.last_name
-   c.email = Faker::Internet.email
-    c.phone_number = Faker::PhoneNumber.cell_phone
-     c.password = Faker::Alphanumeric.alpha(number: 10)
-     c.image = Faker::SlackEmoji.people
-    c.bio = Faker::Lorem.sentence(word_count:5, supplemental: true)
-end
-puts "Creating Artists..."
-Artist.populate 10 do |c|
-  c.first_name = Faker::Name.first_name
-   c.last_name = Faker::Name.last_name
-   c.email = Faker::Internet.email
-    c.phone_number = Faker::PhoneNumber.cell_phone
-     c.password = Faker::Alphanumeric.alpha(number: 10)
-     c.image = Faker::SlackEmoji.people
-    c.bio = Faker::Lorem.sentence(word_count:5, supplemental: true)
-end
-"Fetching all artists"
-artists = Artist.all
-
-
-"Fetching all categories"
-categories = Category.all
-
-"Fetching all clients"
-clients = Client.all
-
-puts "Creating Artist_Category table..."
+# puts "Creating Clients..."
+# Client.populate 6 do |c|
+#   c.first_name = Faker::Name.first_name
+#    c.last_name = Faker::Name.last_name
+#    c.email = Faker::Internet.email
+#     c.phone_number = Faker::PhoneNumber.cell_phone
+#      c.password = Faker::Alphanumeric.alpha(number: 10)
+#      c.image = Faker::SlackEmoji.people
+#     c.bio = Faker::Lorem.sentence(word_count:5, supplemental: true)
+# end
+# puts "Creating Artists..."
+# Artist.populate 10 do |c|
+#   c.first_name = Faker::Name.first_name
+#    c.last_name = Faker::Name.last_name
+#    c.email = Faker::Internet.email
+#     c.phone_number = Faker::PhoneNumber.cell_phone
+#      c.password = Faker::Alphanumeric.alpha(number: 10)
+#      c.image = Faker::SlackEmoji.people
+#     c.bio = Faker::Lorem.sentence(word_count:5, supplemental: true)
+# end
+# "Fetching all artists"
+# artists = Artist.all
 
 
-ArtistCategory.populate 10 do |a| 
+# "Fetching all categories"
+# categories = Category.all
 
-    a.artist_id =  artists.sample
-    a.category_id = categories.sample
+# "Fetching all clients"
+# clients = Client.all
 
-end
+# puts "Creating Artist_Category table..."
 
-puts "Created ArtistCategory"
 
-Review.populate 10 do |r|
-    r.star = Faker::Number.between(from: 1, to: 5)
-    r.artist_id = artists.sample
-    r.client_id = clients.sample
-end
+# ArtistCategory.populate 10 do |a| 
 
-"Creating Requests"
+#     a.artist_id =  artists.sample
+#     a.category_id = categories.sample
 
-Request.populate 20 do |r|
-  r.name = Faker::Lorem.word
-  r.description = Faker::Lorem.sentence(word_count: 10)
+# end
 
-  r.price = 50
-  r.start_date = DateTime.now
-  r.client_id = clients.sample
-  r.artist_id = artists.sample
-  r.category_id = categories.sample
+# puts "Created ArtistCategory"
 
-end
+# Review.populate 10 do |r|
+#     r.star = Faker::Number.between(from: 1, to: 5)
+#     r.artist_id = artists.sample
+#     r.client_id = clients.sample
+# end
 
-"Fetching all requests"
-requests = Request.all
+# "Creating Requests"
 
-"Creating Comments"
+# Request.populate 20 do |r|
+#   r.name = Faker::Lorem.word
+#   r.description = Faker::Lorem.sentence(word_count: 10)
 
-Comment.populate 10 do |c|
-    c.title = Faker::Lorem.word
-    c.content = Faker::Lorem.sentence(word_count: 3, supplemental: true)
-    c.client_id = clients.sample
-    c.request_id = requests.sample
-end
+#   r.price = 50
+#   r.start_date = DateTime.now
+#   r.client_id = clients.sample
+#   r.artist_id = artists.sample
+#   r.category_id = categories.sample
+
+# end
+
+# "Fetching all requests"
+# requests = Request.all
+
+# "Creating Comments"
+
+# Comment.populate 10 do |c|
+#     c.title = Faker::Lorem.word
+#     c.content = Faker::Lorem.sentence(word_count: 3, supplemental: true)
+#     c.client_id = clients.sample
+#     c.request_id = requests.sample
+# end
+
+Client.create(
+  id: 1,
+  first_name: "Lun",
+  last_name: "Nice",
+  email: "Lun@gmail.com",
+  password: 12345,
+  phone_number: "604-729-0794",
+)
+Client.create(
+  id: 2,
+  first_name: "Ji",
+  last_name: "Nice",
+  email: "Ji@gmail.com",
+  password: 12345,
+  phone_number: "604-222-2222",
+)
+Client.create(
+  id: 3,
+  first_name: "Adam",
+  last_name: "Nice",
+  email: "Adam@gmail.com",
+  password: 12345,
+  phone_number: "604-333-3333",
+)
+Client.create(
+  id: 4,
+  first_name: "Senay",
+  last_name: "Nice",
+  email: "Senay@gmail.com",
+  password: 12345,
+  phone_number: "604-444-4444",
+)
+
+Artist.create(
+  id: 1,
+  first_name: "Lun",
+  last_name: "Nice",
+  email: "Lun@gmail.com",
+  password: 12345,
+  phone_number: "604-729-0794",
+)
+Artist.create(
+  id: 2,
+  first_name: "Ji",
+  last_name: "Nice",
+  email: "Ji@gmail.com",
+  password: 12345,
+  phone_number: "604-323-0451",
+)
+Artist.create(
+  id: 3,
+  first_name: "client3",
+  last_name: "Nice",
+  email: "test3@gmail.com",
+  password: 12345,
+  phone_number: "778-124-1946",
+)
+Artist.create(
+  id: 4,
+  first_name: "client4",
+  last_name: "Nice",
+  email: "test4@gmail.com",
+  password: 12345,
+  phone_number: "225-151-7747",
+)
+
+Category.create(
+  id: 1,
+  name: "Guitar",
+)
+Category.create(
+  id: 2,
+  name: "Art",
+)
+Category.create(
+  id: 3,
+  name: "HandyCraft"
+)
+
+test = Request.create(
+  name: "Golden Guitar",
+  image: "https://cdn.mos.cms.futurecdn.net/Ge25ccbyKQ76Et9bBjFnxk-1024-80.jpg.webp",
+  description: "made by maple wood",
+  start_date: nil,
+  expected_finish_date: "2021-08-31",
+  actual_finish_date: nil,
+  price: 20,
+  client_id: 1,
+  artist_id: nil,
+  category_id: 1,
+)
+Request.create(
+  name: "paint",
+  image:
+    "https://images.glaciermedia.ca/polopoly_fs/1.24212567.1601479674!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/kamloops-art-page-2.jpg",
+  description: "urgent request",
+  start_date: nil,
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: nil,
+  price: 40,
+  client_id: 2,
+  artist_id: nil,
+  category_id: 2,
+)
+Request.create(
+  name: "guitar",
+  image:
+    "https://cdn.mos.cms.futurecdn.net/Ge25ccbyKQ76Et9bBjFnxk-1024-80.jpg.webp",
+  description: "made by maple wood",
+  start_date: "2021-01-01",
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: nil,
+  price: 30,
+  client_id: 1,
+  artist_id: 1,
+  category_id: 1,
+)
+Request.create(
+  name: "Golden Guitar",
+  image:
+    "https://cdn.mos.cms.futurecdn.net/Ge25ccbyKQ76Et9bBjFnxk-1024-80.jpg.webp",
+  description: "made by maple wood",
+  start_date: nil,
+  expected_finish_date: "2021-08-31",
+  actual_finish_date: nil,
+  price: 20,
+  client_id: 1,
+  artist_id: nil,
+  category_id: 1,
+)
+Request.create(
+  name: "guitar",
+  image:
+    "https://www.takamine.com/templates/default/images/g90.png",
+  description: "made by maple wood",
+  start_date: "2021-01-01",
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: "2021-03-01",
+  price: 30,
+  client_id: 1,
+  artist_id: 1,
+  category_id: 1,
+)
+Request.create(
+  name: "guitar",
+  image:
+    "https://online.berklee.edu/takenote/wp-content/uploads/2020/07/learn_acoustic_blues_guitar_article_image.jpg",
+  description: "made by maple wood",
+  start_date: "2021-01-01",
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: "2021-03-01",
+  price: 30,
+  client_id: 1,
+  artist_id: 1,
+  category_id: 1,
+)
+Request.create(
+  name: "guitar",
+  image:
+    "https://ewww.music-lessons.ca/wp-content/uploads/2012/04/Acoustic-Guitar.jpeg?strip=all&lossy=1&quality=90&ssl=1",
+  description: "made by maple wood",
+  start_date: "2021-01-01",
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: "2021-03-01",
+  price: 30,
+  client_id: 1,
+  artist_id: 1,
+  category_id: 1,
+)
+Request.create(
+  name: "guitar",
+  image:
+    "https://cdn.shopify.com/s/files/1/3098/9898/products/esp-v-50-blk-ltd-electric-guitar-black-item-type-solid-body-guitars-manufacturer-on-sale-price-250-499-fleet-pro-sound_279_1024x1024@2x.jpg?v=1606346733",
+  description: "made by maple wood",
+  start_date: "2021-01-01",
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: "2021-03-01",
+  price: 30,
+  client_id: 1,
+  artist_id: 1,
+  category_id: 1,
+)
+Request.create(
+  name: "guitar",
+  image:
+    "https://cdn.shopify.com/s/files/1/3098/9898/products/esp-v-50-blk-ltd-electric-guitar-black-item-type-solid-body-guitars-manufacturer-on-sale-price-250-499-fleet-pro-sound_279_1024x1024@2x.jpg?v=1606346733",
+  description: "made by maple wood",
+  start_date: "2021-01-01",
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: "2021-03-01",
+  price: 30,
+  client_id: 1,
+  artist_id: 1,
+  category_id: 1,
+)
+Request.create(
+  name: "guitar",
+  image:
+    "https://www.metmuseum.org/-/media/images/art/collection-landing-page/collection-highlights/fragment-queen.jpg?as=1&la=en&mh=848&mw=1344&hash=792C7AC097CDFD3D161801517B047CBF",
+  description: "made by maple wood",
+  start_date: "2021-01-01",
+  expected_finish_date: "2021-02-01",
+  actual_finish_date: "2021-03-01",
+  price: 30,
+  client_id: 1,
+  artist_id: 4,
+  category_id: 1,
+)
 
